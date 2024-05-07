344684127b06edc5a868363760e6ffe8990b4019
pabvlov <pablojavierprietocepeda@gmail.com>
2024-05-07 19:55:18 -0400

    routing y primer componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeModule } from './home/home.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadChildren: () => HomeModule },
+];
